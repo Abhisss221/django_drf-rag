2d0616e2beb96e2a70aae6799b93e9ea3d0bfbcd
Abhisss221 <abhishekchhabra221@gmail.com>
2026-01-17 23:56:45 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HealthCheck(APIView):
+    def get(self, request):
+        return Response({
+            "status": "ok",
+            "message": "Backend is running"
+        })
+
